83f3eaf38fa57cf710a2a69e05459f0a1d09adb7
Andrew Daly <andrew123daly@gmail.com>
2024-11-04 21:19:26 -0500

    static_hands_v2 events seem workable

--- static_hands_v2.py
+++ static_hands_v2.py
@@ -1,59 +1,62 @@
 import keyboard
 import time
-import string
 
-def print_chord(letter):
-    print(f"j + {letter} chord detected")
-    if letter == 'a':
-        keyboard.send('ctrl+a')
-    if letter == 'b':
+# Define a threshold in seconds for detecting near-simultaneous key presses
+TIME_THRESHOLD = 0.2  # 200 milliseconds
+
+# Track the last time 'j' and 'k' were pressed
+last_j_press = time.time()
+last_k_press = time.time()
+
+def j_k_chord():
+    print("jk chord detected")
+
+# Function to handle 'j' key press
+def on_j_press(event):
+    global last_j_press, last_k_press
+
+    print('last j press: ' + str(last_j_press))
+    # Block 'k' key while 'j' is pressed
+    keyboard.block_key('k')
+    print("'k' is blocked")
+
+    # Check if 'k' was pressed recently within the threshold
+    if last_j_press - last_k_press < TIME_THRESHOLD:
+        if last_j_press > last_k_press:
+            j_k_chord()
+            last_k_press = 0
+            last_j_press = 0
+    try:
+        keyboard.unblock_key('k')
+    except ValueError:
         pass
-    if letter == 'c':
-        keyboard.send('ctrl+c')
-    if letter == 'd':
-        keyboard.send('left')
-    if letter == 'e':
-        keyboard.send('ctrl+e')
-    if letter == 'f':
-        keyboard.send('right')
-    if letter == 'g':
-        keyboard.send('ALT+TAB')
-    if letter == 'i':
-        keyboard.send('up')
-    if letter == 'k':
-        keyboard.send('down')
-    if letter == 'l':
-        keyboard.send('ctrl+l')
-    if letter == 'o':
-        keyboard.send(keyboard.KEY_UP)
-    if letter == 'p':
-        keyboard.send(keyboard.KEY_DOWN)
-    if letter == 'n':
-        keyboard.send('ctrl+n')
-    if letter == 'r':
-        keyboard.send('ctrl+r')
-    if letter == 's':
-        keyboard.send('ctrl+s')
-    if letter == 't':
-        keyboard.send('ctrl+t')
-    # ~j & v:: Send("{CTRL Down}{v}{CTRL Up}")
-    # ~j & w:: Send("{CTRL Down}{w}{CTRL Up}")
-    # ~j & x:: Send("{CTRL Down}{x}{CTRL Up}")
-    # ~j & z:: Send("{CTRL Down}{z}{CTRL Up}")
-    # ~j & SPACE::Send("{BACKSPACE}")
-
-def handler():
-    print('handler called')
-    if keyboard.is_pressed('k'):
-        keyboard.send('down')
+    last_j_press = time.time()  # Update 'j' press timestamp
+    return False
 
 
-#keyboard.block_key('j')
-keyboard.on_press_key('j', handler, suppress=True)
+# Function to handle 'j' key release
+# def on_j_release(event):
+#     # Unblock 'k' when 'j' is released
+#     keyboard.unblock_key('k')
+#     print("'k' is unblocked")
 
-# in this configuration, j doesn't get hooked
-# keyboard.block_key('j')
-# keyboard.hook_key('j', handler)
+
+# Function to handle 'k' key press
+def on_k_press(event):
+    global last_k_press, last_j_press
+    last_k_press = time.time()  # Update 'k' press timestamp
+    print('last k press: ' + str(last_k_press))
+    # # Check if 'j' was pressed recently within the threshold
+    # if last_k_press - last_j_press < TIME_THRESHOLD:
+    #     j_k_chord()
+
+    return False  # Suppress 'k' from being sent to input
+
+
+# Hook 'j' and 'k' keys with their respective handlers and suppress them
+keyboard.hook_key('j', on_j_press, suppress=True)
+keyboard.hook_key('k', on_k_press, suppress=True)
+#keyboard.on_release_key('j', on_j_release)
 
 # Keep the program running
 keyboard.wait('esc')  # Program ends when 'Esc' is pressed
